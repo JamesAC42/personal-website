f0d59be87859306659209fec1756eb2ec2982e5d
James Crovo <jamescrovo450@gmail.com>
2023-10-18 02:01:00 -0500

    Fix resume alt text positioning

--- client/src/components/sections/Resume.jsx
+++ client/src/components/sections/Resume.jsx
@@ -5,7 +5,7 @@ import "../../stylesheets/Resume.css";
 const Resume = () => {
   return (
     <Section centered={true}>
-        <div className="resume-container">
+        <div className="resume-container center-child">
             <object data="/JamesCrovoResume.pdf" type="application/pdf" width="100%" height="100%">
                 <p>Unable to display PDF file. <a href="/JamesCrovoResume.pdf">Download</a> instead.</p>
             </object>
