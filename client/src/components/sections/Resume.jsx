868644590003de1a7acb4d49734f0c25fa38f2bf
James Crovo <jamescrovo450@gmail.com>
2023-10-18 01:56:28 -0500

    Tweaks for mobile

--- client/src/components/sections/Resume.jsx
+++ client/src/components/sections/Resume.jsx
@@ -1,12 +1,15 @@
 import React from 'react';
 import Section from '../Section';
+import "../../stylesheets/Resume.css";
 
 const Resume = () => {
   return (
-    <Section>
-        <object data="/JamesCrovoResume.pdf" type="application/pdf" width="100%" height="100%">
-            <p>Unable to display PDF file. <a href="/JamesCrovoResume.pdf">Download</a> instead.</p>
-        </object>
+    <Section centered={true}>
+        <div className="resume-container">
+            <object data="/JamesCrovoResume.pdf" type="application/pdf" width="100%" height="100%">
+                <p>Unable to display PDF file. <a href="/JamesCrovoResume.pdf">Download</a> instead.</p>
+            </object>
+        </div>
     </Section>
   );
 };
